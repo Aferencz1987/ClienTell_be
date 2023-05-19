f9d0c366772060153f618d0f76c6833a5c13b22c
Alex Ferencz <alex.ferencz6@gmail.com>
2023-05-19 14:49:06 -0600

    PR template change CORS update module name change

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DateEmRateEmBe
+module ClientellBe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
